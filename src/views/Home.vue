0359dbe5128e97fa8f33bf7d7a34ce025d4d5af6
AndreyBS1 <andrey.babenkov.00@gmail.com>
2021-07-03 15:52:32 +0300

    Modal added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,11 @@
 <template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+  <modal v-if="showModal" @close="showModal = false">
+    <template v-slot:header>
+      <h3>custom header</h3>
+    </template>
+  </modal>
+
   <NotesList/>
   <AddNote/>
 </template>
@@ -6,13 +13,20 @@
 <script>
 import NotesList from "@/components/NotesList";
 import AddNote from "@/components/AddNote";
+import Modal from "@/components/Modal";
 
 export default {
   name: "Home",
   components: {
+    Modal,
     NotesList,
     AddNote
   },
+  data() {
+    return {
+      showModal: false,
+    }
+  }
 }
 </script>
 
